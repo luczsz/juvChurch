c447dbf3b93cf7ce23736145aacff8a85985f7e4
Lucas Souza <francisco.lucs@gmail.com>
2023-05-14 12:54:48 -0300

    GENESIS - ROTAS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Ionicons, MaterialIcons, AntDesign } from '@expo/vector-icons';
 
 //pages centrais
 import Home from '../pages/Home';
@@ -7,14 +10,83 @@ import Home from '../pages/Home';
 //pages gerais
 import Bible from '../pages/Bible';
 import Maps from '../pages/Maps';
+import { theme } from '../global/theme';
 
 export default function Routes() {
     
     const AppStack = createNativeStackNavigator();
+    const TabStack = createBottomTabNavigator();
 
+    function Tabs(){
+        return(
+            <TabStack.Navigator
+                screenOptions={{
+                    tabBarShowLabel: false,
+                    tabBarStyle:{
+                        position: 'absolute',
+                        backgroundColor: '#121212',
+
+                        borderTopWidth: 0,
+
+                        //bottom: 14,
+                        elevation: 0,
+                        //borderRadius: 4,
+                        height: 60,
+                                    
+                    }
+                }}
+            >
+                <TabStack.Screen 
+                    name='Inicio' 
+                    component={Home} 
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size, focused}) => {
+                            if(focused){
+                            return <AntDesign name="home" size={24} color={theme.colors.primary} />
+                        }
+                            return <AntDesign name="home" size={24} color="#DDD" />
+    
+                        }
+                    }}
+                />
+                
+                <TabStack.Screen 
+                    name='Biblia' 
+                    component={Bible}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size, focused}) => {
+                            if(focused){
+                            return <AntDesign name="book" size={24} color={theme.colors.primary} />
+                        }
+                            return <AntDesign name="book" size={24} color="#DDD" />
+    
+                        }
+                    }} 
+                />
+                <TabStack.Screen 
+                    name='Maps' 
+                    component={Maps}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size, focused}) => {
+                            if(focused){
+                            return <Ionicons name="location" size={24} color={theme.colors.primary} />
+                        }
+                            return <Ionicons name="location-outline" size={24} color="#DDD" />
+    
+                        }
+                    }}
+                />
+            </TabStack.Navigator>
+        )
+    };
+
+    
     return (
         <AppStack.Navigator>
-            <AppStack.Screen name='Home' component={Home} options={{ headerShown: false, }} />
+            <AppStack.Screen name='Home' component={Tabs} options={{ headerShown: false, }} />
             <AppStack.Screen name='Bible' component={Bible} />
             <AppStack.Screen name='Map' component={Maps} />
         </AppStack.Navigator>
